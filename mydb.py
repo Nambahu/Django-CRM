0c90e37f2c3ad76eccf662943a212a27bf0246e5
Nambahu <homateni@gmail.com>
2023-04-28 10:24:37 +0200

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,22 @@
+# Install Mysql on your computer
+# https://dev.mysql.com/downloads/installer/
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = 'localhost',
+	user = 'root',
+	passwd = 'Hom@1960'
+
+	)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Creat a database
+cursorObject.execute('CREATE DATABASE Pyrad')
+
+print('ALL DONE!')
